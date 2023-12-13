bcab73842112eb961434f1bd19770d41ba427edc
Mariano Vargas <marianoluisvargas@gmail.com>
2023-12-13 19:25:20 -0300

    Feat(models/category, models/index): modelo de category creado e incorporado

--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, DataTypes) => {
   const Note = sequelize.define("Note", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     title: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -13,10 +18,6 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: false,
     },
   });
-
-  Note.associate = (models) => {
-    Note.belongsToMany(models.Category, { through: "NoteCategory" });
-  };
 
   return Note;
 };
