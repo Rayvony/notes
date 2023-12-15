4b830bd64750ec5441e5206b64ec0d703782d0b9
Mariano Vargas <91701599+Rayvony@users.noreply.github.com>
2023-12-15 14:39:07 -0300

    Delete start_app.sh

--- start_app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Navigate to notes_backend and start the backend server
-cd notes-backend
-npm start &
-
-# Navigate to notes_frontend and start the frontend development server
-cd ../notes-frontend
-npm run dev
